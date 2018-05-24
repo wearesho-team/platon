effe9f03bc870835b11c669b204156ab7e4331c6
Alexander Letnikow <reclamme@gmail.com>
2018-05-24 17:14:15 +0300

    Implement LanguageTransaction

---
+++ src/LanguageTransaction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wearesho\Bobra\Platon;
+
+use Wearesho\Bobra\Payments;
+
+/**
+ * Class LanguageTransaction
+ * @package Wearesho\Bobra\Platon
+ */
+class LanguageTransaction extends Transaction implements Payments\HasLanguage
+{
+    use LanguageConfigTrait;
+
+    public function __construct(
+        int $service,
+        float $amount,
+        string $type,
+        string $description,
+        array $info = [],
+        string $currency = 'UAH',
+        string $formId = null,
+        string $language = ConfigInterface::LANGUAGE_UA
+    ) {
+        parent::__construct($service, $amount, $type, $description, $info, $currency, $formId);
+        $this->setLanguage($language);
+    }
+}

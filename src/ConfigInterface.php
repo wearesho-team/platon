4bfc3e60a2847d24b278ee30215a876b9ac68e06
Beton <aleksa.yagich@gmail.com>
2018-05-29 14:44:23 +0300

    Fix ukrainian language code

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -9,7 +9,7 @@ namespace Wearesho\Bobra\Platon;
 interface ConfigInterface
 {
     public const LANGUAGE_RU = 'ru';
-    public const LANGUAGE_UA = 'ua';
+    public const LANGUAGE_UA = 'uk';
 
     public const PAYMENT_URL = 'https://secure.platononline.com/';
 

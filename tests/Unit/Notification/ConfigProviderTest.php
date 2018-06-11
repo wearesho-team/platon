4aeeec0ec74d58c7ec652920aee4a897eaf8b2f1
Beton <aleksa.yagich@gmail.com>
2018-06-11 12:36:38 +0300

    Fix tests

--- tests/Unit/Notification/ConfigProviderTest.php
+++ tests/Unit/Notification/ConfigProviderTest.php
@@ -15,19 +15,35 @@ class ConfigProviderTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $config = new Config('test', 'test pass', '');
+        $config = new Config('key', 'pass', 'CC');
         $provider = new Notification\ConfigProvider([$config]);
-        $targetConfig = $provider->provide($config->getKey());
+
+        $targetConfig = $provider->provide(
+            'order',
+            '411111****1111',
+            '6ee9a37176e1859eb070aedcc4c6a937'
+        );
         $this->assertEquals($targetConfig->getPass(), $config->getPass());
     }
 
     /**
-     * @expectedException \Wearesho\Bobra\Platon\Notification\UnsupportedMerchantException
+     * @expectedException \Wearesho\Bobra\Platon\Notification\InvalidSignException
+     */
+    public function testEmpty(): void
+    {
+        $provider = new Notification\ConfigProvider();
+        $provider->provide('order', 'card', 'sign');
+    }
+
+    /**
+     * @expectedException \Wearesho\Bobra\Platon\Notification\InvalidSignException
      */
     public function testMissing(): void
     {
-        $provider = new Notification\ConfigProvider();
-        $provider->provide('missed key');
+        $provider = new Notification\ConfigProvider([
+            new Config('test', 'test', 'CC'),
+        ]);
+        $provider->provide('order', 'invalidPass', 'invalidSign');
     }
 
     /**

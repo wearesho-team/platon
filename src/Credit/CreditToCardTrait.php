d73726a7b38a6f9a8ad2242c16faf871e781448e
Beton <aleksa.yagich@gmail.com>
2018-07-02 15:00:06 +0300

    Implement new interfaces from bobra-payments

--- src/Credit/CreditToCardTrait.php
+++ src/Credit/CreditToCardTrait.php
@@ -2,66 +2,15 @@
 
 namespace Wearesho\Bobra\Platon\Credit;
 
+use Wearesho\Bobra\Payments\Credit;
+
 /**
  * Trait CreditToCardTrait
  * @package Wearesho\Bobra\Platon\Credit
  */
 trait CreditToCardTrait
 {
-    /** @var string */
-    private $cardToken;
-
-    /** @var string */
-    protected $id;
-
-    /** @var int */
-    protected $amount;
-
-    /** @var string|null */
-    protected $currency;
-
-    /** @var string|null */
-    protected $description;
-
-    /**
-     * @inheritdoc
-     */
-    public function getCardToken(): string
-    {
-        return $this->cardToken;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getId(): string
-    {
-        return $this->id;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getAmount(): int
-    {
-        return $this->amount;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getCurrency(): string
-    {
-        return $this->currency ?? 'UAH';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
+    use Credit\TransferTrait;
 
     protected function setCardToken(string $cardToken): self
     {

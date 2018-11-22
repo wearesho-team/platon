f60ffc9410f76be1bd6a35b52200669c040d3ca0
Alexander Letnikow <reclamme@gmail.com>
2018-11-22 14:23:02 +0200

    Implement info ConfigInterface by entity and environment config

---
+++ src/Info/EnvironmentConfig.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Wearesho\Bobra\Platon\Info;
+
+use Horat1us\Environment;
+
+/**
+ * Class EnvironmentConfig
+ * @package Wearesho\Bobra\Platon\Info
+ */
+class EnvironmentConfig extends Environment\Config implements ConfigInterface
+{
+    public function __construct(string $keyPrefix = 'PLATON_INFO_')
+    {
+        parent::__construct($keyPrefix);
+    }
+
+    public function getPublicKey(): string
+    {
+        return $this->getEnv('PUBLIC_KEY');
+    }
+
+    public function getPrivateKey(): string
+    {
+        return $this->getEnv('PRIVATE_KEY');
+    }
+}

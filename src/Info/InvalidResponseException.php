bf1387cb4b787b5456e0155c73b0d3ff93aa220f
Alexander Letnikow <reclamme@gmail.com>
2018-11-22 14:11:49 +0200

    Add InvalidResponseException for Info

---
+++ src/Info/InvalidResponseException.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wearesho\Bobra\Platon\Info;
+
+use Throwable;
+
+/**
+ * Class InvalidResponseException
+ * @package Wearesho\Bobra\Platon\Info
+ */
+class InvalidResponseException extends \RuntimeException
+{
+    /** @var array */
+    protected $body;
+
+    public function __construct(string $message = "", int $code = 0, array $body, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->body = $body;
+    }
+
+    public function getBody(): array
+    {
+        return $this->body;
+    }
+
+    public function __toString(): string
+    {
+        return parent::__toString() . PHP_EOL . "Body: {$this->body}";
+    }
+}

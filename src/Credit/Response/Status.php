3d8d3b5d7483311ebfe141e460acb7caa0a03a0a
Alexander Letnikow <reclamme@gmail.com>
2019-02-27 15:13:51 +0200

    Add isWaiting method to C2C response

---
+++ src/Credit/Response/Status.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wearesho\Bobra\Platon\Credit\Response;
+
+/**
+ * Class Status
+ * @package Wearesho\Bobra\Platon\Credit\Response
+ */
+interface Status
+{
+    public const WAITING = 'WAITING';
+}
